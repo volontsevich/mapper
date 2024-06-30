7db02b89cff70d6a51e9dd2dd2c9232934672494
volontsevich <volontsevich@gmail.com>
2024-06-30 17:42:45 -0400

    Update service-worker.js

--- assets/service-worker.js
+++ assets/service-worker.js
@@ -28,27 +28,35 @@ self.addEventListener('install', event => {
 });
 
 self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => {
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request).then(
-                    response => {
-                        if (!response || response.status !== 200 || response.type !== 'basic') {
-                            return response;
-                        }
-                        const responseToCache = response.clone();
-                        caches.open(CACHE_NAME)
-                            .then(cache => {
-                                cache.put(event.request, responseToCache);
-                            });
+    const requestUrl = new URL(event.request.url);
+
+    if (requestUrl.origin !== location.origin) {
+        event.respondWith(
+            fetch(event.request).catch(() => caches.match(event.request))
+        );
+    } else {
+        event.respondWith(
+            caches.match(event.request)
+                .then(response => {
+                    if (response) {
                         return response;
                     }
-                );
-            })
-    );
+                    return fetch(event.request).then(
+                        response => {
+                            if (!response || response.status !== 200 || response.type !== 'basic') {
+                                return response;
+                            }
+                            const responseToCache = response.clone();
+                            caches.open(CACHE_NAME)
+                                .then(cache => {
+                                    cache.put(event.request, responseToCache);
+                                });
+                            return response;
+                        }
+                    );
+                })
+        );
+    }
 });
 
 self.addEventListener('activate', event => {
